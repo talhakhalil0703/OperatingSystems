d04f8a78896b1dc1cd6c972abbcd235f84d0077c
talhakhalil0703 <tkhalil0703@gmail.com>
2021-09-30 20:04:02 -0600

    Assignment 1

---
+++ Assignment_1/palindrome.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+import sys
+
+def main():
+    max_pali = ""
+    while True:
+        line = sys.stdin.readline()
+        if len(line) == 0: break
+        words = line.split()
+        for word in words:
+            if len(word) <= len(max_pali):
+                continue
+            if word.lower() == word.lower()[::-1]:
+                max_pali = word
+    print(f"Longest palindrome: {max_pali}")
+
+main()
