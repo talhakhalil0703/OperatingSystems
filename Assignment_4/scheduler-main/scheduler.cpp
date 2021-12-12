2f07f9622c00e1869f484aabfd04476bbe3d71b7
talhakhalil0703 <tkhalil0703@gmail.com>
2021-12-11 20:18:54 -0700

    A4Q2 Done

--- Assignment_4/scheduler-main/scheduler.cpp
+++ Assignment_4/scheduler-main/scheduler.cpp
@@ -69,14 +69,99 @@ bool time_slice_exceeded(std::vector<Process> & processes, int64_t quantum)
     return false;
 }
 
-bool cpu_idle_and_job_available(std::vector<Process> & processes, std::vector<int> & seq)
+bool all_processes_in_rq_have_start_time(std::vector<Process> & processes)
 {
-    if (cpu == -1 && rq.size() > 0) {
+    for (int64_t id : rq) {
+        if (processes[id].start_time == -1) return false;
+    }
+    return true;
+}
+
+bool check_for_idle_time_skip(
+    std::vector<Process> & processes, std::vector<int> & seq, int64_t max_seq_len, int64_t quantum)
+{
+    bool do_it = true;
+
+    int64_t smallest_burst_finish = INT64_MAX;
+    int64_t cpu_id = -1;
+    for (int64_t id : rq) {
+        if (remaining_bursts[id] < quantum) { do_it = false; }
+        if (remaining_bursts[id] < smallest_burst_finish) {
+            smallest_burst_finish = remaining_bursts[id];
+            cpu_id = id;
+        }
+    }
+
+    bool process = false;
+    if (new_process_id < processes.size()) {
+
+        if ((rq.size() + 1) * quantum < (processes[new_process_id].arrival_time - cpu_time)) {
+            do_it = true;
+            process = true;
+            if ((processes[new_process_id].arrival_time - cpu_time) < smallest_burst_finish) {
+                smallest_burst_finish = processes[new_process_id].arrival_time - cpu_time;
+            }
+        } else {
+            do_it = false;
+        }
+    }
+
+    // Shortest time recieved
+
+    // int64_t N = std::floor(1.0 * smallest_burst_finish / quantum);
+    int64_t N = (process) ? (smallest_burst_finish / (quantum * rq.size())) - 1
+                          : (smallest_burst_finish / (quantum)) - 1;
+
+    if (N <= 0) do_it = false;
+
+    if (do_it) {
+        // Go up until loop before and modify everything
+        int64_t time_spent = 0;
+        for (int64_t id : rq) {
+            remaining_bursts[id] -= N * quantum;
+            time_spent += N * quantum;
+        }
+        cpu_time += time_spent;
+        curr_process_bursts = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int64_t id : rq) {
+                if (seq.size() >= max_seq_len) return true;
+                if (seq.back() != id) {
+                    seq.push_back(id);
+                } else {
+                    if (rq.size() == 1) return true;
+                }
+            }
+        }
+
         cpu = rq.front();
         rq.erase(rq.begin());
-        // If first time starting the process note it's start time
-        if (processes[cpu].start_time == -1) processes[cpu].start_time = cpu_time;
-        if (seq.back() != cpu) seq.push_back(cpu);
+        return true;
+    }
+    return false;
+}
+
+bool cpu_idle_and_job_available(
+    std::vector<Process> & processes, std::vector<int> & seq, int64_t quantum, int64_t max_seq_len)
+{
+    if (cpu == -1 && rq.size() > 0) {
+        if (all_processes_in_rq_have_start_time(processes)) {
+            if (check_for_idle_time_skip(processes, seq, max_seq_len, quantum)) return true;
+
+            cpu = rq.front();
+            rq.erase(rq.begin());
+            // If first time starting the process note it's start time
+            if (processes[cpu].start_time == -1) processes[cpu].start_time = cpu_time;
+            if (seq.back() != cpu && seq.size() < max_seq_len) seq.push_back(cpu);
+        } else {
+            cpu = rq.front();
+            rq.erase(rq.begin());
+            // If first time starting the process note it's start time
+            if (processes[cpu].start_time == -1) processes[cpu].start_time = cpu_time;
+            if (seq.back() != cpu && seq.size() < max_seq_len) seq.push_back(cpu);
+        }
+
         return true;
     }
     return false;
@@ -94,10 +179,10 @@ void simulate_rr(
         if (current_job_is_done(processes)) continue;
         if (time_slice_exceeded(processes, quantum)) continue;
         if (new_process_arrive(processes)) continue;
-        if (cpu_idle_and_job_available(processes, seq)) continue;
+        if (cpu_idle_and_job_available(processes, seq, quantum, max_seq_len)) continue;
 
         if (cpu == -1) { // If idling note that.
-            if (seq.back() != -1) seq.push_back(cpu);
+            if (seq.back() != -1 && seq.size() < max_seq_len) seq.push_back(cpu);
             if (rq.size() == 0 && new_process_id < processes.size()) {
                 cpu_time = processes[new_process_id].arrival_time;
             } else {
