0589c540bab27bd6d08d861bb26c1bf693d51a63
talhakhalil0703 <tkhalil0703@gmail.com>
2021-12-12 11:27:45 -0700

    A5Q2 Init

---
+++ Assignment_5/fatsim-main/fatsim.cpp
@@ -0,0 +1,16 @@
+// -------------------------------------------------------------------------------------
+// this is the only file you need to edit
+// -------------------------------------------------------------------------------------
+//
+// (c) 2021, Pavol Federl, pfederl@ucalgary.ca
+// Do not distribute this file.
+
+#include "fatsim.h"
+#include <cstdio>
+
+// reimplement this function
+std::vector<long> fat_check(const std::vector<long> & fat)
+{
+  return {1,1,5};
+}
+
